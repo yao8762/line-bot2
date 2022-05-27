70ac4fe758ea5b4b005c5fb63739f95c08f064a7
yao8762 <hot665992002@gmail.com>
2022-05-27 09:54:38 +0800

    upadta msg

--- app.py
+++ app.py
@@ -37,9 +37,21 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = '很抱歉，我看不懂'
+
+    if msg in ['hi', 'Hi', 'HI']:
+        r = '哈囉'
+    elif msg == '你吃飯了嗎?':
+        r == '還沒'
+    elif msg == '你是誰':
+        r == '我是機器人'
+    elif '訂位' in msg:
+        r = '您想訂位，是嗎?'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
